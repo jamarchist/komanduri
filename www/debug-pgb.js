4f2706ab39f7803922d04333419e94aad1b8f02a
Ryan Gray <jamesryangray@gmail.com>
2014-03-13 16:40:03 -0700

    making sure my event handler is attaching

--- www/debug-pgb.js
+++ www/debug-pgb.js
@@ -1,5 +1,9 @@
 ﻿document.querySelector('body').addEventListener('click', function () {
+    console.log('body clicked.');
+
     for (var messageIndex = 0; messageIndex < window.logMessages.length; messageIndex++) {
         alert(window.logMessages[messageIndex]);
     }
+
+    console.log('messages shown.');
 });
